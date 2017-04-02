ffa16d98ac703890385eb00fd5abeaa8d3d1ad60
Arjan Jassal <arjan.jassal.27@gmail.com>
2017-04-02 14:38:02 -0700

    create CurrentMaxes component and some basic styles

---
+++ src/current-max/CurrentMax.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CurrentMax extends Component {
+  render() {
+    return (
+      <h1>Your current 1 rep maxes</h1>
+    );
+  }
+}
+
+export default CurrentMax;
