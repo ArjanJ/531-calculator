a1498730ad9de5cde3ffda17e34bb58cb40d5704
Arjan Jassal <arjan.jassal.27@gmail.com>
2017-04-05 22:13:10 -0700

    integrate firebase with google oauth and getting sign in/out working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 import CurrentMax from './current-max/CurrentMax';
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
     super(props);
     this.handleCurrentMaxChange = this.handleCurrentMaxChange.bind(this);
     this.handleCurrentMaxSubmit = this.handleCurrentMaxSubmit.bind(this);
+    this.signOut = this.signOut.bind(this);
     this.state = {
       currentMaxes: {
         benchPress: '',
@@ -16,7 +18,23 @@ class App extends Component {
       },
       cycles: null,
       unit: 'lb',
+      user: null,
     };
+  }
+
+  authCheck() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        // No user is signed in.
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.authCheck();
   }
 
   calculateCycle(currentMax) {
@@ -24,11 +42,11 @@ class App extends Component {
     const cycleBase = [0.65, 0.7, 0.75, 0.4];
     for (let i = 0; i < 4; i++) {
       cycle.push([
-        Math.floor(currentMax * (cycleBase[i]) / 5) * 5,
-        Math.floor(currentMax * (cycleBase[i] + 0.1) / 5) * 5,
-        Math.floor(currentMax * (cycleBase[i] + 0.2) / 5 ) * 5,
-        Math.floor(currentMax * (cycleBase[i] + 0.25) / 5 ) * 5,
-        Math.floor(currentMax * (cycleBase[i] + 0.3) / 5 ) * 5,
+        { weight: Math.floor(currentMax * (cycleBase[i]) / 5) * 5 },
+        { weight: Math.floor(currentMax * (cycleBase[i] + 0.1) / 5) * 5 },
+        { weight: Math.floor(currentMax * (cycleBase[i] + 0.2) / 5 ) * 5 },
+        { joker: true, weight: Math.floor(currentMax * (cycleBase[i] + 0.25) / 5 ) * 5 },
+        { joker: true, weight: Math.floor(currentMax * (cycleBase[i] + 0.3) / 5 ) * 5 },
       ]);
     }
     return cycle;
@@ -55,11 +73,25 @@ class App extends Component {
     });
   }
 
+  signOut() {
+    firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      this.authCheck();
+    }).catch(error => {
+      // An error happened.
+    });
+  }
+
   render() {
-    const { currentMaxes, cycles } = this.state;
+    const { signIn } = this.props;
+    const { currentMaxes, cycles, user } = this.state;
     const reps = [5, 3, 1, 5];
+
     return (
       <div className="App">
+        {user === null && <button onClick={signIn}>Sign in</button>}
+        {user !== null && <h5>{user.displayName}</h5>}
+        {user !== null && <button onClick={this.signOut}>Sign out</button>}
         <CurrentMax
           currentMaxes={currentMaxes}
           handleChange={this.handleCurrentMaxChange}
@@ -70,10 +102,24 @@ class App extends Component {
               <div style={{ marginBottom: '30px' }}>
                 <div>{cycle.lift}</div>
                 <div style={{ display: 'flex' }}>{cycle.cycle.map((c, i) => {
+                  if (i === 2) {
+                    let reps2 = 7;
+                    return (
+                      <div style={{ marginRight: '15px' }}>
+                        <span>week {i + 1}</span>
+                        {c.map((d, j) => {
+                          if (j <= 2) { reps2 -= 2; }
+                          return <div>{d.weight}x{reps2}</div>;
+                        })}
+                      </div>
+                    );
+                  }
                   return (
                     <div style={{ marginRight: '15px' }}>
                       <span>week {i + 1}</span>
-                      {c.map(d => <div>{d}x{reps[i]}</div>)}
+                      {c.map(d => {
+                        return <div>{d.weight}x{reps[i]}</div>;
+                      })}
                     </div>
                   );
                 })}</div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase';
 import App from './App';
 import './index.css';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyBFR1cE2u34m1p_q5gTiPIUBpTUdf7Fbwk',
+  authDomain: 'logger-8b13a.firebaseapp.com',
+  databaseURL: 'https://logger-8b13a.firebaseio.com',
+  storageBucket: 'logger-8b13a.appspot.com',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const signIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider)
+    .then(result => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const token = result.credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+  }).catch(error => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = error.credential;
+  });
+};
+
+
 ReactDOM.render(
-  <App />,
+  <App signIn={signIn} />,
   document.getElementById('root')
 );
